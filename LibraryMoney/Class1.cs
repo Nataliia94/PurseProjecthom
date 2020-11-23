0bb45d40f5c5bc25667e2061ad1df3d15dae6ef5
Natalia <kornchak365@gmail.com>
2020-11-23 23:15:00 +0200

    Add library money

---
+++ LibraryMoney/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryMoney
+{
+    public class Money
+    {
+        public string Amount { get; set; }
+    }
+}
